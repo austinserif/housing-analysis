cb718943f3a0fdd6cc64d99ce05e899196aca351
Austin Serif <austin@sans-serif.io>
2022-11-14 23:03:49 -0800

    adds mash specific env var configuration

---
+++ configs/mashvisor.config.js
@@ -0,0 +1,6 @@
+require('dotenv').config();
+module.exports = {
+    MASH_BASE_URL: process.env.MASH_BASE_URL,
+    MASH_VERSION: process.env.MASH_VERSION,
+    MASH_API_KEY: process.env.MASH_API_KEY
+}
